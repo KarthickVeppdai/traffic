b42b193ee16de6e80b7cd7eb493f86da6a034d94
KarthickVeppdai <karthickjava1997@gmail.com>
2025-01-06 14:42:11 +0530

    Only messaging samplem d

--- src/main/java/com/traffic/traffic/RabbitMqConfiguration.java
+++ src/main/java/com/traffic/traffic/RabbitMqConfiguration.java
@@ -4,12 +4,14 @@ package com.traffic.traffic;
 
 import org.springframework.amqp.core.*;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
 
 @Configuration
+@EnableRabbit
 public class RabbitMqConfiguration {
 
 
